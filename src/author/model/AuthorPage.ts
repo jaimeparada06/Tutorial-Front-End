b28cc54889adfddf7cffa8790e7b4d371cfe5433
Jaime Parada <109216598+jaimeparada06@users.noreply.github.com>
2023-04-05 11:51:39 +0200

    Delete src directory

--- src/author/model/AuthorPage.ts
+++
@@ -1,8 +0,0 @@
-import { Pageable } from "../../core/model/page/Pageable";
-import { Author } from "./Author";
-
-export class AuthorPage {
-    content: Author[];
-    pageable: Pageable;
-    totalElements: number;
-}
--- src/loan/model/LoanPage.ts
+++
@@ -1,8 +0,0 @@
-import { Pageable } from "../../core/model/page/Pageable";
-import { Loan } from "./Loan";
-
-export class LoanPage {
-    content: Loan[];
-    pageable: Pageable;
-    totalElements: number;
-}
